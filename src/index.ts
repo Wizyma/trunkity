c58cd2a8da8a4b36ac74e3daaab79c4f6c6cd719
Andre Gomes Ramalho <jsparanoguy@gmail.com>
2020-05-18 09:39:55 +0200

    feat: 🎸 add workspace tree

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-#!/usr/bin/env ts-node-script
+#!/usr/bin/env node
 
 import { resolve } from 'path';
 import execa from 'execa';
 import chalk from 'chalk';
+import program from 'commander';
+import fse from 'fs-extra';
 
 const { log } = console;
 
@@ -17,11 +19,25 @@ class Configuration {
   isAGitRepository: boolean;
   rootPackage: PackageJSON;
   isMonorepo: boolean;
-
+  root: string;
+  workspaceInfo: {
+    name: string;
+    path: string
+  }[]
+  workspacesTree: {
+    name: string,
+    path: string,
+    workspaceDependencies: string[],
+    workspaceDependenciesByType: {
+      dev: string[],
+      peer: string[],
+      direct: string[]
+    },
+  }[]
+  
   private getRootPackage(): PackageJSON {
     if(this.isAGitRepository) {
-      const root = execa.commandSync('git rev-parse --show-toplevel').stdout;
-      const packageJSON: PackageJSON = require(resolve(root, 'package.json'));
+      const packageJSON: PackageJSON = require(resolve(this.root, 'package.json'));
   
       return packageJSON;
     }
@@ -53,12 +69,94 @@ class Configuration {
 
   constructor() {
     this.isAGitRepository = execa.commandSync('git rev-parse --is-inside-work-tree').stdout === 'true';
-    this.rootPackage = this.getRootPackage();
-    this.isMonorepo = this.checkIsMonorepo();
 
-    if(!this.isMonorepo || !this.isAGitRepository) {
+    if(!this.isAGitRepository) {
       process.exit(0)
     }
+
+    this.root = execa.commandSync('git rev-parse --show-toplevel').stdout;
+    this.isMonorepo = this.checkIsMonorepo();
+    this.rootPackage = this.getRootPackage();
+
+    if(!this.isMonorepo) {
+      process.exit(0)
+    }
+  
+    this.workspaceInfo = this
+      .getWorkspacesInfo()
+      // @ts-ignore
+      .map(({ json, ...rest }) => ({
+        ...rest
+      }))
+    this.workspacesTree = this.getWorkspaceTree();
+  }
+
+  getWorkspacesInfo() {
+    const workspaces = this.rootPackage.workspaces;
+    const dirsPath = workspaces.map(workspace => {
+      if(workspace.includes('*')) {
+        return `${this.root}/${workspace.replace('*', '')}`
+      }
+      return `${this.root}/${workspace}`
+    });
+
+    return dirsPath
+      .map(dir => {
+        return fse.readdirSync(dir).map(packagePath => {
+          const fullPath = `${dir}/${packagePath}`;
+          if(fullPath.includes('//')) {
+            return fullPath.replace('//', '/')
+          }
+    
+          return fullPath
+        });
+      })
+      .reduce((a, b) => {
+        return a.concat(b)
+      }, [])
+      .map(packagePath => {
+        const json = fse.readJSONSync(`${packagePath}/package.json`, { throws: false });
+        if(json) {
+          return {
+            json,
+            name: json.name,
+            path: packagePath,
+          }
+        }
+
+        return null
+      })
+      .filter(file => file !== null)
+  }
+
+  getWorkspaceTree() {
+    return this.getWorkspacesInfo()
+      // @ts-ignore
+      .map(({ name, json, path }) => {
+        const depsByType = {
+          dev: [
+            ...Object.keys(json?.devDependencies ?? {}),
+          ].filter(dep => dep.includes(this.rootPackage.name)),
+          peer: [
+            ...Object.keys(json?.peerDependencies ?? {}),
+          ].filter(dep => dep.includes(this.rootPackage.name)),
+          direct: [
+            ...Object.keys(json?.dependencies ?? {}),
+          ].filter(dep => dep.includes(this.rootPackage.name)),
+        }
+        const deps = [
+          ...Object.keys(json?.devDependencies ?? {}),
+          ...Object.keys(json?.peerDependencies ?? {}),
+          ...Object.keys(json?.dependencies ?? {}),
+        ].filter(dep => dep.includes(this.rootPackage.name))
+
+        return {
+          name,
+          path,
+          workspaceDependencies: deps,
+          workspaceDependenciesByType: depsByType,
+        }
+      })
   }
 }
 
@@ -70,7 +168,7 @@ class MonorepoTooling extends Configuration {
       return changes;
     }
 
-    log(chalk.grey('No changed detected'));
+    log(chalk.grey('No changes detected'));
     log(chalk.grey('Exiting...'));
     process.exit(0)
   }
@@ -80,8 +178,14 @@ class MonorepoTooling extends Configuration {
   }
 
   main() {
-    const changes = this.getChanged();
-    log(changes)
+    program
+      .option('-a, --affected')
+      .parse(process.argv)
+    
+    if(program.affected) {
+      console.log(this.getChanged())
+      this.getWorkspaceTree()
+    }
   }
 }
 
